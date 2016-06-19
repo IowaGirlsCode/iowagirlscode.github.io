1c7f98f5f775edf19977133d284adc842278dea2
Kelsey Hrubes <kelseyhrubes@gmail.com>
2016-06-19 10:36:03 -0700

    add timeline section

--- js/main.js
+++ js/main.js
@@ -1,76 +1,26 @@
 jQuery(document).ready(function($){
-	var gallery = $('.cd-gallery'),
-		foldingPanel = $('.cd-folding-panel'),
-		mainContent = $('.cd-main');
-	/* open folding content */
-	gallery.on('click', 'a', function(event){
-		event.preventDefault();
-		openItemInfo($(this).attr('href'));
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
 	});
 
-	/* close folding content */
-	foldingPanel.on('click', '.cd-close', function(event){
-		event.preventDefault();
-		toggleContent('', false);
-	});
-	gallery.on('click', function(event){
-		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
-		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
-	})
-
-	function openItemInfo(url) {
-		var mq = viewportSize();
-		if( gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
-			/* if content is visible above the .cd-gallery - scroll before opening the folding panel */
-			$('body,html').animate({
-				'scrollTop': gallery.offset().top
-			}, 100, function(){ 
-	           	toggleContent(url, true);
-	        }); 
-	    } else if( gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height()  && mq != 'mobile' ) {
-			/* if content is visible below the .cd-gallery - scroll before opening the folding panel */
-			$('body,html').animate({
-				'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
-			}, 100, function(){ 
-	           	toggleContent(url, true);
-	        });
-		} else {
-			toggleContent(url, true);
-		}
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
 	}
 
-	function toggleContent(url, bool) {
-		if( bool ) {
-			/* load and show new content */
-			var foldingContent = foldingPanel.find('.cd-fold-content');
-			foldingContent.load(url+' .cd-fold-content > *', function(event){
-				setTimeout(function(){
-					$('body').addClass('overflow-hidden');
-					foldingPanel.addClass('is-open');
-					mainContent.addClass('fold-is-open');
-				}, 100);
-				
-			});
-		} else {
-			/* close the folding panel */
-			var mq = viewportSize();
-			foldingPanel.removeClass('is-open');
-			mainContent.removeClass('fold-is-open');
-			
-			(mq == 'mobile' || $('.no-csstransitions').length > 0 ) 
-				/* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
-				? $('body').removeClass('overflow-hidden')
-				
-				: mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-					$('body').removeClass('overflow-hidden');
-					mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-				});
-		}
-		
-	}
-
-	function viewportSize() {
-		/* retrieve the content value of .cd-main::before to check the actua mq */
-		return window.getComputedStyle(document.querySelector('.cd-main'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
 	}
 });
